0b8c852966b63d1d87be06e40931b5b6cddaa81f
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-07 16:23:26 +0100

    initial push

--- vorobei_3/ContentView.swift
+++ vorobei_3/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var onTap: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        HStack(spacing: 0) {
+            Button {
+                withAnimation(.bouncy) {
+                    onTap.toggle()
+                } completion: {
+                     onTap = false
+                }
+
+            } label: {
+                HStack(spacing: 0) {
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: onTap ? 50 : 0, height: onTap ? 50 : 0)
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: onTap ? 0.5 : 50, height: onTap ? 0.5 : 50)
+
+                }
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            }
         }
-        .padding()
     }
 }
 
